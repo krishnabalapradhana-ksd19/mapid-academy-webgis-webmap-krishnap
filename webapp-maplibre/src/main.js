284a3c99f30a37b298f6bb571fed13afcbf1aa54
Krishna Balapradhana <krishna.balapradhana@gmail.com>
2026-07-15 22:26:13 +0700

    Spongebob added

--- webapp-maplibre/src/main.js
+++ webapp-maplibre/src/main.js
@@ -1,5 +1,7 @@
-import { Map } from 'maplibre-gl';
-// import { addKotaLayer, addPulauLayer } from './layers/vector';
+import 'maplibre-gl/dist/maplibre-gl.css';import { Map } from 'maplibre-gl';
+import naturalEarthData from './assets/ne_populated.geojson?url';
+import pulauKalimantan from './assets/Kalimantan.geojson?url';
+// { addKotaLayer, addPulauLayer } from './layers/vector';
 // import { addSpongebobImage } from './layers/raster';
 
 const mapElement = document.createElement('div');
@@ -14,30 +16,65 @@ const map = new Map({
   zoom: 1
 });
 
-const data = { 
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {
-        "name": "Jakarta"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          106.8404846,
-          -6.2177536
-        ]
-      }
-    }
-  ]
-}
+// const data = { 
+//   "type": "FeatureCollection",
+//   "features": [
+//     {
+//       "type": "Feature",
+//       "properties": {
+//         "name": "Jakarta"
+//       },
+//       "geometry": {
+//         "type": "Point",
+//         "coordinates": [
+//           106.8404846,
+//           -6.2177536
+//         ]
+//       }
+//     }
+//   ]
+// }
 
 
 map.on('load', () => {
 map.addSource('kota', {
   type: 'geojson',
-  data: data
+  data: naturalEarthData
+});
+map.addSource('pulau', {
+  type: 'geojson',
+  data: pulauKalimantan
+});
+
+map.addSource('spongebob', {
+  type: 'image',
+  url: 'https://static.wikia.nocookie.net/spongebob/images/6/6b/Spongebob_happy_stock_art_1.octet-stream.png',
+  coordinates: [
+    [-43.76, 27.22],  // kiri atas  (barat, utara)
+    [-16.24, 27.22],  // kanan atas (timur, utara)
+    [-16.24, 12.78],  // kanan bawah (timur, selatan)
+    [-43.76, 12.78]   // kiri bawah (barat, selatan)
+  ]
+});
+
+map.addLayer({
+  id: 'pulau-kalimantan',
+  type: 'fill',
+  source: 'pulau',
+  paint: {
+    'fill-color': '#00FF00',
+    'fill-outline-color': '#000000',
+    'fill-opacity': 0.5
+  }
+});
+
+map.addLayer({
+  id: 'spongebob-image',
+  type: 'raster', 
+  source: 'spongebob',
+  paint: {
+    'raster-opacity': 0.8
+  }
 });
 
 map.addLayer({
@@ -47,10 +84,12 @@ source: 'kota',
   paint: {
     'circle-radius': 10,
     'circle-color': '#FF0000',
-    'circle-stroke-width': ,
+    'circle-stroke-width': 2,
     'circle-stroke-color' : 'black'
   }
 });
+
+
   // addKotaLayer(map);
   // addPulauLayer(map);
   // addSpongebobImage(map);
